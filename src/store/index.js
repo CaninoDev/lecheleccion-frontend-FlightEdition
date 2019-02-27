3b6ee5ab9c88b0763caa9be36bb51b28d732fe36
Ian G Canino <ian.canino@gmail.com>
2019-02-27 13:19:48 -0500

    finalize redux boilerplate, implement basic news container

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
-import rootReducer from 'reducers'
+import news from '../news/NewsMiddleware'
+import rootReducer from '../reducers'
 
 var middlewares = []
 
 export default (initialState) => {
   middlewares.push(thunk)
   middlewares.push(logger)
+    middlewares.push(news)
   return createStore(
     rootReducer,
     initialState,

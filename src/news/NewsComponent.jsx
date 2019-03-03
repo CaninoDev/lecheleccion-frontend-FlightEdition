f5afae72212bcb2b2f5d9b3cae0ab7b5bb4f2723
IanGC <ian@thecommittedbug.io>
2019-03-02 22:50:09 -0500

    add proptype test and change prop name in NewsComponent

--- src/news/NewsComponent.jsx
+++ src/news/NewsComponent.jsx
@@ -7,23 +7,19 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 
 const NewsComponent = props => {
-  return (
-    <Card>
-      <CardMedia
-        image={props.articleData.urlToImage}
-      >
-      </CardMedia>
-      <CardContent>
-        <Typography variant='caption' gutterBottom>{props.articleData.publication_date}</Typography>
-        <Typography>{props.articleData.title}</Typography>
-      </CardContent>
-    </Card>
-  )
+    return (
+        <Card key={props.article.ID}>
+            <CardContent>
+                <CardMedia image={props.article.URLToImage}/>
+                <Typography variant='caption' gutterBottom>{props.article.PublicationDate}</Typography>
+                <Typography>{props.article.Title}</Typography>
+            </CardContent>
+        </Card>
+    )
 }
 
 NewsComponent.propTypes = {
-  articleData:PropTypes.object
+    article:PropTypes.object
 }
 
 export default NewsComponent
-

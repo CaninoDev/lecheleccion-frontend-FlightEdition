0c1acd18641fa64b720848422fd625db7737dcfb
IanGC <ian@thecommittedbug.io>
2019-03-02 22:52:18 -0500

    create index for reducers folder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux'
+import { news } from './news'
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+    news
+})
 
 export default rootReducer

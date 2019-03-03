2dd084180c01e00dc4d515979251bffa399c1cc4
IanGC <ian@thecommittedbug.io>
2019-03-02 22:54:39 -0500

    add middleware for redux dev tool

--- src/store/index.js
+++ src/store/index.js
@@ -2,19 +2,19 @@ import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
-import rootReducer from '../reducers'
+import rootReducer from 'reducers'
 
 var middlewares = []
 
 export default (initialState) => {
   middlewares.push(thunk)
   middlewares.push(logger)
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return createStore(
     rootReducer,
     initialState,
-    compose(
-      applyMiddleware(...middlewares),
-      window.devToolsExtension ? window.devToolsExtension() : f => f
+    composeEnhancers(
+      applyMiddleware(...middlewares)
     )
   )
 }

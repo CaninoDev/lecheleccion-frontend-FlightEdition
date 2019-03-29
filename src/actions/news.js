4466ce7fa16717e9bf11402fdafbbd3aec03f20a
Canino Developments <ian@thecommittedbug.io>
2019-03-29 07:17:48 -0400

    create a fetchNews action

--- src/actions/news.js
+++ src/actions/news.js
@@ -1,21 +1,37 @@
 import { NEWS_REQUEST, NEWS_SUCCESS, NEWS_FAILURE } from 'news/constants'
 
-export function requestNews () {
+function requestNews () {
     return {
         type: NEWS_REQUEST
     }
 }
 
-export function receivedNews (data) {
+function receivedNews (data) {
     return {
     type: NEWS_SUCCESS,
     data: data
     }
 }
 
-export function failureNews (error) {
+function failureNews (error) {
     return {
         type: NEWS_FAILURE,
         errorMessage: error
+    }
+}
+
+export default function fetchNews() {
+    return (dispatch) => {
+        return fetch('/api/articles', {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ type: 'quantity', payload: 50 }),
+        }).then((response) => {
+            if (!response.ok) {
+                return throw response.json()
+            } else {
+                return dispatch(receivedNews(response.json()))
+            }
+        }).catch(error => dispatch(failureNews(error)))
     }
 }

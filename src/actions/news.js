a58a8d5a72687097cea49bbd84473204b24ae8de
IanGC <ian@thecommittedbug.io>
2019-03-29 10:47:26 -0400

    fix broken fetchNews() and use new cross-fetch dependency

--- src/actions/news.js
+++ src/actions/news.js
@@ -1,3 +1,4 @@
+import fetch from 'cross-fetch'
 import { NEWS_REQUEST, NEWS_SUCCESS, NEWS_FAILURE } from 'news/constants'
 
 function requestNews () {
@@ -20,18 +21,11 @@ function failureNews (error) {
     }
 }
 
-export default function fetchNews() {
-    return (dispatch) => {
-        return fetch('/api/articles', {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ type: 'quantity', payload: 50 }),
-        }).then((response) => {
-            if (!response.ok) {
-                return throw response.json()
-            } else {
-                return dispatch(receivedNews(response.json()))
-            }
-        }).catch(error => dispatch(failureNews(error)))
+export default function () {
+    return dispatch => {
+        dispatch(requestNews)
+        return fetch('/api/articles')
+           .then(response => response.json())
+           .then(data => dispatch(receivedNews(data)))
     }
 }

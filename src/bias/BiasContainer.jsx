5cebea33bb5aa6f943f5c88f88acc47722e57b02
IanGC <ian@thecommittedbug.io>
2019-04-04 13:05:59 -0400

    create biascomponent

--- src/bias/BiasContainer.jsx
+++ src/bias/BiasContainer.jsx
@@ -1,7 +1,9 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core/styles'
+import {connect} from 'react-redux'
 
+import { BiasComponent } from './BiasComponent'
 
 const styles = themes => ({
   chart: {
@@ -9,22 +11,40 @@ const styles = themes => ({
     bottom: 0,
     right: 0,
     width: '300px',
-    border: '3px solid #73AD21'
+    height: '300px',
+    background: '#73AD21'
   }
 })
 
 class BiasContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: this.props.loading,
+      data: this.props.data
+    }
+  }
+
   render() {
     const { classes } = this.props
     return (
        <div className={classes.chart}>
-         Hello!
+         {!this.props.loading && <BiasComponent data={this.props.data} />}
        </div>
     )
   }
 }
-export default withStyles(styles)(BiasContainer)
+
+const mapStateToProps = state => ({
+  data: state.biases.data,
+  loading: state.biases.loading
+})
+
+export default withStyles(styles)(connect(mapStateToProps)(BiasContainer))
 
 BiasContainer.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  bias: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
+  data: PropTypes.object.isRequired
 }
